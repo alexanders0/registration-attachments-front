183f397c1c61d09932956732a117b0ffe3ff990d
alexanders0 <alexandersn059@gmail.com>
2021-11-02 00:53:33 -0500

    basename updated in router

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -9,7 +9,7 @@ import {
 
 function AppUI() {
   return (
-    <Router basename='/repo-name'>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         <Route path="/:encodedUrl" children={<RequirementsList />} />
